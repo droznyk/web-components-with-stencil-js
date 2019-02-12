57e78115e4732c8e753409c9a82272a3fb4f3384
Patryk Stepien <droznyk@gmail.com>
2019-02-12 13:04:51 +0100

    Use references instead of @Element decorator

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -1,4 +1,4 @@
-import { Component, State, Element } from "@stencil/core";
+import { Component, State } from "@stencil/core";
 
 import { AV_API_KEY } from '../../global/global';
 
@@ -9,13 +9,16 @@ import { AV_API_KEY } from '../../global/global';
 })
 // NDCPQJ7Y0RHO8PJG
 export class StockPrice {
-  @Element() el: HTMLElement;
+  stockInput: HTMLInputElement;
+
+  // @Element() el: HTMLElement;
 
   @State() fetchedPrice: number;
 
   onFetchStockPrice(event: Event) {
     event.preventDefault();
-    const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    // const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    const stockSymbol = this.stockInput.value;
     fetch(
       `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`
     )
@@ -33,7 +36,7 @@ export class StockPrice {
   render () {
     return [
       <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input id="stock-symbol"/>
+        <input id="stock-symbol" ref={el => this.stockInput = el}/>
         <button type="submit">Fetch</button>
       </form>,
       <div>
