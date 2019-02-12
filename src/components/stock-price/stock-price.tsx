8234d344c22850fe798b42b792fff54f594244d4
Patryk Stepien <droznyk@gmail.com>
2019-02-12 14:22:41 +0100

    Add validation for form

--- src/components/stock-price/stock-price.tsx
+++ src/components/stock-price/stock-price.tsx
@@ -7,40 +7,79 @@ import { AV_API_KEY } from '../../global/global';
   styleUrl: './stock-price.css',
   shadow: true
 })
-// NDCPQJ7Y0RHO8PJG
+
 export class StockPrice {
   stockInput: HTMLInputElement;
 
   // @Element() el: HTMLElement;
 
   @State() fetchedPrice: number;
+  @State() stockUserInput: string;
+  @State() stockInputValid = false;
+  @State() error: string;
 
-  onFetchStockPrice(event: Event) {
-    event.preventDefault();
-    // const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
-    const stockSymbol = this.stockInput.value;
-    fetch(
-      `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`
-    )
-      .then(res => {
-        return res.json();
-      })
-      .then(parsedRes => {
-        this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  onUserInput = (event: Event) => {
+    this.stockUserInput = (event.target as HTMLInputElement).value;
+    if (this.stockUserInput.trim() !== '') {
+      this.stockInputValid = true;
+    } else {
+      this.stockInputValid = false;
+    }
   }
 
-  render () {
+  onFetchStockPrice = async (event: Event) => {
+    event.preventDefault();
+    // const stockSymbol = (this.el.shadowRoot.querySelector('#stock-symbol') as HTMLInputElement).value;
+    try {
+      const stockSymbol = this.stockInput.value;
+      const response = await fetch(
+        `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`
+      )
+
+      const parsed_response = await response.json()
+      if (!parsed_response['Global Quote']['05. price']) {
+        throw new Error('Invalid symbol!');
+      }
+      this.error = null;
+      this.fetchedPrice = + parsed_response['Global Quote']['05. price'];
+    } catch (error) {
+      this.error = error.message
+    }
+    // fetch(
+    //   `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${AV_API_KEY}`
+    // )
+    //   .then(res => {
+    //     return res.json();
+    //   })
+    //   .then(parsedRes => {
+    //     if (!parsedRes['Global Quote']['05. price']) {
+    //       throw new Error('Invalid symbol!');
+    //     }
+    //     this.fetchedPrice = +parsedRes['Global Quote']['05. price'];
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
+  }
+
+  render() {
+    let dataContent = <p>Please enter a symbol.</p>;
+    if (this.error) {
+      dataContent = <p>{this.error}</p>
+    }
+    if (this.fetchedPrice) {
+      dataContent = <p>Price: ${this.fetchedPrice}</p>
+    }
     return [
-      <form onSubmit={this.onFetchStockPrice.bind(this)}>
-        <input id="stock-symbol" ref={el => this.stockInput = el}/>
-        <button type="submit">Fetch</button>
+      <form onSubmit={this.onFetchStockPrice}>
+        <input id="stock-symbol"
+          ref={el => this.stockInput = el}
+          value={this.stockUserInput}
+          onInput={this.onUserInput} />
+        <button type="submit" disabled={!this.stockInputValid}>Fetch</button>
       </form>,
       <div>
-        <p>Price: ${this.fetchedPrice}</p>
+        {dataContent}
       </div>
     ];
   }
